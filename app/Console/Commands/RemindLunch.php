3c77d63c9f32ef223f6f211a9e18d9f7fc0e285e
hieu.nguyen.van <nguyen.van.hieu@framgia.com>
2017-11-11 22:30:46 +0700

    Add remind schedule

---
+++ app/Console/Commands/RemindLunch.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace app\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+use wataridori\ChatworkSDK\ChatworkRoom;
+use wataridori\ChatworkSDK\ChatworkSDK;
+use wataridori\ChatworkSDK\ChatworkApi;
+
+class RemindLunch extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'remind:lunch';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Remind lunch';
+
+    protected $chatworkRoom;
+    protected $chatworkApi;
+    protected $roomId = 89867570;
+
+    public function __construct()
+    {
+        parent::__construct();
+        ChatworkSDK::setApiKey(env("CHATWORK_API_KEY"));
+        $this->chatworkRoom = new ChatworkRoom($this->roomId);
+        $this->chatworkApi = new ChatworkApi();
+    }
+
+    public function handle()
+    {
+        $message = "It's time to lunch. Get your ass off the chair!";
+        $members = $this->chatworkRoom->getMembers();
+        $me = $this->chatworkApi->me();
+        $otherMembers = collect($members)->filter(function($member) use ($me) {
+            return $member->account_id != $me['account_id'];
+        });
+        $this->chatworkRoom->sendMessageToList($otherMembers, $message, false, false);
+    }
+
+}
