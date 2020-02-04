6f02d5820b31e7f671ef68d18e216a2d104ddb66
hieunv95 <hieu2395@gmail.com>
2020-02-05 03:59:12 +0700

    Add feature: Delete BOT message in command.

--- app/Http/Controllers/ChatworkController.php
+++ app/Http/Controllers/ChatworkController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Requests;
+use Illuminate\Support\Arr;
 use Illuminate\Http\Request;
 use App\Api\ChatworkExtend\ChatworkApi;
 use App\Api\ChatworkExtend\ChatworkRoom;
@@ -12,6 +13,7 @@ class ChatworkController extends Controller
     const WEBHOOK_KEYS = [
         'room_id' => 'webhook_event.room_id',
         'from_account_id' => 'webhook_event.from_account_id',
+        'to_account_id' => 'webhook_event.to_account_id',
         'message_id' => 'webhook_event.message_id',
         'message_body' => 'webhook_event.body',
     ];
@@ -59,22 +61,41 @@ class ChatworkController extends Controller
      */
     private function replyToMentionMessage()
     {
-        $this->chatworkRoom->sendMessage($this->buildReplyMentionMessage());
+        $this->chatworkRoom->sendMessage($this->buildAnswerMessage());
     }
 
     /**
-     * Build reply text for mention message.
+     * Build an answer message for replying the mention message.
      *
      * @return string
      */
-    private function buildReplyMentionMessage()
+    private function buildAnswerMessage()
     {
         $fromAccountId = $this->getWebhookVal('from_account_id');
         $messageId = $this->getWebhookVal('message_id');
         $messageBody = $this->getWebhookVal('message_body');
+        $originalBotName = data_get($this->chatworkApi->me(), 'name') ?? '';
+        $botName = preg_replace('/[^\pL\s]+/u', '', $originalBotName);
+        $botName = trim($botName);
+        $mainMentionContent = preg_replace('/\[.*?]/', '', $messageBody);
+        $mainMentionContent = preg_replace('/\s+/', ' ', $mainMentionContent);
+        $mainMentionContent = preg_replace('/' . $originalBotName . '/i', ' ', $mainMentionContent);
+        $mainMentionContent = trim($mainMentionContent);
 
-        return "[rp aid={$fromAccountId} to={$this->roomId}-{$messageId}]" . PHP_EOL
-            . $this->getAnswerFromSimi($messageBody);
+        switch (true) {
+            case $mainMentionContent === '':
+                $questionMarks = ['(?)', '(??)', '(???)'];
+                $mainAnswerContent =  $questionMarks[mt_rand(0, count($questionMarks) - 1)];
+                break;
+            case strtolower($mainMentionContent) === 'del-msg':
+                $mainAnswerContent = $this->deleteAnsweredMessages();
+                break;
+            default:
+                $mainAnswerContent = $this->getAnswerFromSimi($mainMentionContent, $botName);
+                break;
+        }
+
+        return "[rp aid={$fromAccountId} to={$this->roomId}-{$messageId}]" . PHP_EOL . $mainAnswerContent;
     }
 
     /**
@@ -97,25 +118,12 @@ class ChatworkController extends Controller
     /**
      * Get random anwser from Simsimi API.
      *
-     * @param  string  $mentionMesssage
-     *
+     * @param  string  $utext
+     * @param  string  $botName
      * @return mixed|string|string[]|null
      */
-    private function getAnswerFromSimi($mentionMesssage = '')
+    private function getAnswerFromSimi($utext = '', $botName = '')
     {
-        $originalBotName = data_get($this->chatworkApi->me(), 'name') ?? '';
-        $botName = preg_replace('/[^\pL\s]+/u', '', $originalBotName);
-        $botName = trim($botName);
-        $utext = preg_replace('/\[.*?]/', '', $mentionMesssage);
-        $utext = preg_replace('/\s+/', ' ', $utext);
-        $utext = preg_replace('/' . $originalBotName . '/i', ' ', $utext);
-        $utext = trim($utext);
-        if ($utext === '') {
-            $questionMarks = ['(?)', '(??)', '(???)'];
-
-            return $questionMarks[mt_rand(0, count($questionMarks) - 1)];
-        }
-
         $answerText = '';
         $headers = [
             'Content-Type' => 'application/json',
@@ -123,7 +131,9 @@ class ChatworkController extends Controller
         ];
         $data = '{
             "utext": "' . $utext . '", 
-            "lang": "vn"
+            "lang": "vn",
+            "country": ["VN"],
+            "atext_bad_prob_max": 0.1
         }';
 
         try {
@@ -135,9 +145,115 @@ class ChatworkController extends Controller
             $answerText = data_get(json_decode($response->body, true), 'atext') ?? '';
             $answerText = preg_replace('/simsimi|símimi|simimi|símini|simi|sisi|sim/i', $botName, $answerText);
         } catch (\Exception $e) {
-            \Log::info($e);
+            \Log::info($e->getMessage());
         }
 
         return in_array($answerText, ['', 'iy']) ? '(think)' : $answerText;
+    }
+
+    /**
+     * Delete the answered messages by BOT.
+     *
+     * @return string
+     */
+    private function deleteAnsweredMessages()
+    {
+        $isAdminMember = $this->isAdminMember();
+        $messageIds = $this->getMessageIndicesInMentionMessage();
+        $messageIdsCount = count($messageIds);
+
+        if (!$messageIdsCount) {
+            return 'Không có tin nhắn nào em xóa được cả (dull)';
+        }
+
+        $deleteErrorCount = 0;
+        $notDeletableCount = 0;
+
+        foreach ($messageIds as $messageId) {
+            try {
+                if ($isAdminMember || $this->isDeletableMessage($messageId)) {
+                    $this->chatworkApi->deleteMessage($this->roomId, $messageId);
+                } else {
+                    $notDeletableCount++;
+                }
+            } catch (\Exception $e) {
+                $deleteErrorCount++;
+                if ($deleteErrorCount === $messageIdsCount) {
+                    \Log::info($e->getMessage());
+                    return 'Chắc có lỗi gì rồi, em không xóa được tin nhắn đâu :(';
+                }
+            }
+        }
+
+        if ($notDeletableCount) {
+            return 'Mội vài tin nhắn chính chủ vào xóa thì hay hơn ạ :v';
+        }
+
+        return 'Em đã xóa tin nhắn rồi nhé ;)';
+    }
+
+    /**
+     * Get message IDs from mention message.
+     *
+     * @return array
+     */
+    private function getMessageIndicesInMentionMessage()
+    {
+        $aid = $this->getWebhookVal('to_account_id');
+        $messageBody = $this->getWebhookVal('message_body');
+
+        $messageIdPattern = '/\[rp aid=' . $aid . '.*to=' . $this->roomId . '-(.*?)]'
+            . '|https:\/\/www.chatwork.com\/#!rid' . $this->roomId . '-(.*?)$/m';
+        preg_match_all($messageIdPattern, $messageBody, $matches);
+
+        $messageIdsFromReply = Arr::wrap($matches[1] ?? []);
+        $messageIdsFromLink = Arr::wrap($matches[2] ?? []);
+        $messageIds = array_merge($messageIdsFromReply, $messageIdsFromLink);
+
+        array_walk($messageIds, function (&$messageId) {
+            $messageId = preg_split('/\s+/', trim($messageId))[0] ?? '';
+            $messageId = preg_replace('/[^0-9]/', '', $messageId);
+        });
+
+        return array_values(array_unique(array_filter($messageIds)));
+    }
+
+    /**
+     * Check if the message is deletable.
+     *
+     * @param $messageId
+     * @return bool
+     */
+    private function isDeletableMessage($messageId)
+    {
+        try {
+            $fromAccountID = $this->getWebhookVal('from_account_id');
+            $messageDetail = $this->chatworkApi->getRoomMessageByMessageId($this->roomId, $messageId);
+
+            return strpos($messageDetail['body'] ?? '', '[rp aid=' . $fromAccountID) !== false;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    /**
+     * Check if the sender is the admin.
+     *
+     * @return bool
+     */
+    public function isAdminMember()
+    {
+        try {
+            $fromAccountID = $this->getWebhookVal('from_account_id');
+            $members = $this->chatworkApi->getRoomMembersById($this->roomId);
+            $isAdminMember = collect($members)
+                ->where('role', 'admin')
+                ->where('account_id', $fromAccountID)
+                ->count();
+
+            return in_array($fromAccountID, explode(',', env('ADMIN_CHATWORK_ID', ''))) || $isAdminMember;
+        } catch (\Exception $e) {
+            return false;
+        }
     }
 }
